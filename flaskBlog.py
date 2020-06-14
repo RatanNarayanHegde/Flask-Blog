dd41872bcec37a1dd28590a2556b1a2a52815ca8
Ratan Hegde <ratanhegde@pop-os.localdomain>
2020-06-14 22:31:49 +0530

    add and style templates

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app= Flask(__name__)
 
+posts = [
+    {
+        'author': 'Ratan Hegde',
+        'title': 'Blog Post 1',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, mollitia?'
+        'date_pub': 'June 14 2020'
+    },
+    {
+        'author': 'Shubham Kumtole',
+        'title': 'Blog Post 2',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, mollitia?'
+        'date_pub': 'June 13 2020'
+    }
+]
+
 @app.route("/")
 def home():
-    return "<h1> Home Page</h1>"
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1> About Page</h1>"
+    return render_template('about.html',title='About')
 
 
 if __name__=='__main__':
