4054e1fd4ffbf836a08c225810396da5ad150165
Ratan Hegde <ratanhegde@pop-os.localdomain>
2020-06-16 23:02:15 +0530

    add profile picture option

--- flaskBlog/forms.py
+++ flaskBlog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from flaskBlog.models import User
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField,BooleanField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 
@@ -30,3 +32,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('username already taken. Choose a different one')
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already taken. Choose a different one')
