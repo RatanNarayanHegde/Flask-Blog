7e237cb92bf121e8e3caf2b2b1ce70df451acbbc
Ratan Hegde <ratanhegde@pop-os.localdomain>
2020-06-21 10:06:36 +0530

    add pagination

--- flaskBlog/routes.py
+++ flaskBlog/routes.py
@@ -9,9 +9,10 @@ from PIL import Image
 
 
 @app.route("/")
+@app.route("/home")
 def home():
     page_no = request.args.get('page',1,type=int)
-    posts= Post.query.paginate(page=page_no,per_page=2)
+    posts= Post.query.order_by(Post.date_posted.desc()).paginate(page=page_no,per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -135,3 +136,12 @@ def delete_post(post_id):
     flash('Your Post has been deleted','success')
     return redirect(url_for('home'))
     
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page_no = request.args.get('page',1,type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts= Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page_no,per_page=5)
+    return render_template('user_posts.html', posts=posts,user=user)
+
